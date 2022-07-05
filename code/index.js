298dc678ce67a406f983f6425df44a1dae22084d
nashalysf <nashalysf@gmail.com>
2022-07-05 17:01:20 -0400

    quizz starting button and rules styled

---
+++ code/index.js
@@ -0,0 +1,42 @@
+/* 
+INSTRUCTIONS
+1. You'll have <span>60 seconds</span> to complete the quizz.
+2. Once you submit the answer you can't change it.
+3. If your answer is wrong, <span> 10 seconds</span> will be reducted from the timer.
+4. You can select answers if the timer goes off.
+5. You can't exit the quizz while playing.
+6. Your highscore will be measured by correct answers.
+
+QUESTIONS & ANSWER
+Commonly used data types DO Not include:
+1. Strings
+2. Booleans
+3. Alerts
+4. Numbers
+
+The condition in an if / else statement is enclosed with:
+1. Quotes
+2. Curly brackets
+3. Parenthesis
+4. Square brackets
+
+Array in JavaScript can be used to store ______.
+1. Numbers and settings
+2. Other arrays 
+3. Booleans
+4. All the above
+
+String values must be enclosed withing ______ when being assigned to variables.
+1. Commas
+2. Curly Brackets
+3. Quotes
+4. Parenthesis
+
+A very useful tool used during development and debugging for printing content to
+the debbuger is:
+1. JavaScript
+2. Terminal/bash
+3. For loops
+4. Console.log
+
+*/
