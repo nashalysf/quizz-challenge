e9ff7b261b8662997e3e1293df51c4f53279ff6e
nashalysf <nashalysf@gmail.com>
2022-07-07 21:27:55 -0400

    working buttons for part 1 to part 2 and part4, and working buttons for part 4 to part 1 and part 2; missing questions functions to store user data and compare user answers to correct answer, and user score with user initials to show on highscore

--- code/script.js
+++ code/script.js
@@ -1,23 +1,44 @@
 //all elements
-const startBtn = document.getElementById('start-btn')
-const highScoreList = document.getElementById("hs-btn")
-const questionElement = document.getElementById('questions')
-const answers = document.getElementById('answers')
-
-let shuffledQuestions, currentQuestion
+const startBtn = document.getElementById('start-btn');
+const highScoreList = document.getElementById("hs-btn");
+const quitBtn = document.getElementById("quit");
+const restartBtn = document.getElementById("restart");
+const part1 = document.getElementById("part1").style.display="block";
+const part2 = document.getElementById("part2").style.display="none";
+const part3 = document.getElementById("part3").style.display="none";
+const part4 = document.getElementById("part4").style.display="none";
 
 startBtn.addEventListener("click", startQuiz)
 highScoreList.addEventListener("click", showHighScores)
+restartBtn.addEventListener('click', restartQuiz)
+
 
 function startQuiz() {
     console.log('started');
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    document.getElementById("part1").style.display="none";
+    document.getElementById("part2").style.display="block";
+    var nextQuestion = document.getElementsByClassName('questions');
+    for (i<0; i < questions.length; i++);
+    const answers = document.getElementsByClassName("answers").style.display="none";
+    if (answers.addEventListener ("click")){
+        nextQuestion
+    }
+    if (userAnswer === correctAnswer){
+        document.getElementById("answer-q1" , "correct").style.backgroundColor="lightgreen";
+    }
+    /*shuffledQuestions = questions.sort(() => Math.random() - .5);
     currentQuestion = 0;
     part2.classList.remove('hide');
-    NextQuestion();
+    NextQuestion();*/
 }
 function showHighScores(){
     console.log(part4);
+    document.getElementById("part1").style.display="none";
+    document.getElementById("part4").style.display="block";
+}
+function restartQuiz(){
+    document.getElementById("part4").style.display="none"
+    document.getElementById("part2").style.display="block";
 }
 function nextQuestion() { 
 showQuestion(shuffledQuestions[currentQuestion])
